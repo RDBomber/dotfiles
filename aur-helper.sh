1be51c3b00973c6c9184a9102350e1b0b70289c0
RDBomber <62676175+RDBomber@users.noreply.github.com>
2024-08-01 18:02:52 -0400

    Small scripts, please do not run them unless you happen to have a completely identical setup or target setup.

---
+++ aur-helper.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
---
+++ install-packages.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+yay -Syu --needed --sudoloop --noconfirm - < packages.txt
